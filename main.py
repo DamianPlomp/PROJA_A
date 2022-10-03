8fc93bc6788cd7a3e728b54beb46b00bb988915f
Damian <damian.plomp@student.hu.nl>
2022-10-03 10:23:22 +0200

    test

---
+++ main.py
@@ -0,0 +1,31 @@
+from datetime import *
+import random
+
+stations = ["Dordrecht", "Delft", "Deventer", "Enschede", "Gouda", "Groningen", "Den Haag"]
+
+def get_information():
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    station = random.choice(stations)
+    message = input("You are at station " + station + ". Please tell us what you thought of your trip. (max 140 characters)\n")
+    if len(message) > 140:
+        print("That message was too long, please try again.")
+        return get_information()
+    anonymous = input("Would you like your review to be anonymous? Type Y for yes, and N for no.\n").lower()
+    if anonymous == 'y':
+        print("You will remain anonymous")
+        name = "Anonymous"
+    elif anonymous == 'n':
+        name = input("Please tell us your name.\n")
+
+    return message, dt_string, name, station
+
+message, dt_string, name, station = get_information()
+
+information = [name, dt_string, station, message]
+
+with open('information.txt', 'a') as f:
+    for info in information:
+        f.write(info)
+        f.write('\n')
+
