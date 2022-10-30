7c96aecd957ae134d1bc0331e6a788abf847879a
Damian <damian.plomp@student.hu.nl>
2022-10-30 16:38:26 +0100

    is_facility()

--- main.py
+++ main.py
@@ -1,11 +1,99 @@
 from datetime import *
 import psycopg2
 import random
+import tkinter
+from tkinter import *
+from PIL import ImageTk, Image
 
 stations = ["Dordrecht", "Delft", "Deventer", "Enschede", "Gouda", "Groningen", "Den Haag"]
 
 logged_in = False
 logged_in_user = None
+buttonIsClicked = False
+station = None
+
+def is_facility(station):
+    facilities = ['station_city', 'country', 'ov_bike', 'elevator', 'toilet', 'park_and_ride']
+
+    connection = psycopg2.connect(user="postgres", password="Plofkip17", host="localhost", database="StationsZuill",
+                                  port="5432")
+    c = connection.cursor()
+
+    query = ("SELECT * FROM station_service WHERE station_city = %s")
+    c.execute(query, station)
+    booleans = c.fetchall()
+    for facility, facility_true in zip(facilities, booleans[0]):
+        if facility_true:
+            print(facility)
+
+    connection.commit()
+    c.close()
+
+def station_window(station):
+    sw = tkinter.Toplevel()
+
+    sw.geometry("1024x538")
+
+    image = Image.open("s_background.png")
+    bg = ImageTk.PhotoImage(image)
+    my_label = Label(sw, image=bg)
+    my_label.place(x=0, y=0)
+
+    message = Label(sw, text='Laat hier je bericht achter over wat je van de reiservaring vond:')
+    message.grid(row=0, column=0)
+
+    message_text = Text(sw, width=40, height=10, pady=10, padx=10)
+    message_text.grid(row=1, column=0, pady=20)
+
+    naam_info = Label(sw, text='Laat hier je naam achter, als je anoniem wilt blijven vul dan "anoniem" in.')
+    naam_info.grid(row=2, column=0)
+
+    name_text = Text(sw, width=40, height=2, pady=10, padx=10)
+    name_text.grid(row=3, column=0, pady=20)
+
+    submit_button = tkinter.Button(sw, text='Submit', command=lambda: insert_message(message_text, name_text, station))
+    submit_button.grid(row=4, column=0, pady=20)
+
+    facilities = Label(sw, text=is_facility(station))
+    facilities.grid(row=5, column=0, columnspan=1, pady=20)
+
+    return message_text, name_text, station
+
+def main_window():
+    window = Tk()
+    window.geometry("1024x538")
+
+    image = Image.open("s_background.png")
+    bg = ImageTk.PhotoImage(image)
+
+    my_label = Label(window, image=bg)
+    my_label.place(x=0, y=0)
+
+    button1 = tkinter.Button(window, text='Dordrecht', width=40, height=10, command=lambda: station_window('Dordrecht'))
+    button1.grid(row=0, column=0, padx=25, pady=10)
+
+    button2 = tkinter.Button(window, text='Delft', width=40, height=10, command=lambda: station_window('Delft'))
+    button2.grid(row=0, column=1, padx=25, pady=10)
+
+    button3 = tkinter.Button(window, text='Deventer', width=40, height=10, command=lambda: station_window('Deventer'))
+    button3.grid(row=0, column=2, padx=25, pady=10)
+
+    button4 = tkinter.Button(window, text='Enschede', width=40, height=10, command=lambda: station_window('Enschede'))
+    button4.grid(row=1, column=0, padx=25, pady=10)
+
+    button5 = tkinter.Button(window, text='Gouda', width=40, height=10, command=lambda: station_window('Gouda'))
+    button5.grid(row=1, column=1, padx=25, pady=10)
+
+    button6 = tkinter.Button(window, text='Groningen', width=40, height=10, command=lambda: station_window('Groningen'))
+    button6.grid(row=1, column=2, padx=25, pady=10)
+
+    button7 = tkinter.Button(window, text='Den Haag', width=40, height=10, command=lambda: station_window('Den Haag'))
+    button7.grid(row=3, column=1, padx=25, pady=10)
+
+    exit_button = tkinter.Button(window, text='Exit', width=40, height=10, command=window.destroy)
+    exit_button.grid(row=3, column=2, padx=25, pady=10)
+
+    window.mainloop()
 
 def register_moderator():
     connection = psycopg2.connect(user="postgres", password="Plofkip17", host="localhost", database="StationsZuill", port="5432")
@@ -17,7 +105,6 @@ def register_moderator():
     c.execute(registered_moderators)
     if username in registered_moderators:
         print("This username has already been taken, please choose a different one.")
-        logged_in_user = username
         return register_moderator()
     else:
         details = (username, email, password)
@@ -60,20 +147,12 @@ def login_moderator(): # Details zijn vooraf al gemaakt (check txt file moderato
     connection.commit()
     c.close()
 
-def insert_message():
+def insert_message(message_text, name_text, station):
     now = datetime.now()
     submissionTimeStamp = now.strftime("%d/%m/%Y %H:%M:%S")
-    station = random.choice(stations)
-    message = input("You are at station " + station + ". Please tell us what you thought of your trip. (max 140 characters)\n>")
-    if len(message) > 140:
-        print("That message was too long, please try again.")
-        return insert_message()
-    anonymous = input("Would you like your review to be anonymous? Type Y for yes, and N for no.\n>").lower()
-    if anonymous == 'y':
-        print("You will remain anonymous")
-        name = "Anonymous"
-    elif anonymous == 'n':
-        name = input("Please tell us your name.\n>")
+    station = station
+    message = message_text.get(1.0, "end-1c")
+    name = name_text.get(1.0, "end-1c")
     information = [message, submissionTimeStamp, name, station, '\n']
 
     with open('information.txt', 'a') as s:
@@ -140,4 +219,4 @@ def menu():
         elif default_menu_choices == 2:
             break
 
-login_moderator()
+main_window()
